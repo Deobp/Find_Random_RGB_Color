a3c612117d563e38b2d2c009be61fcd498afbb03
Sasha <zzsanchozz@gmail.com>
2020-09-15 13:01:23 +0300

    optimize js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,4 @@
-let button1 = document.getElementById('button1');
-let button2 = document.getElementById('button2');
-let button3 = document.getElementById('button3');
-let button4 = document.getElementById('button4');
-let button5 = document.getElementById('button5');
-let button6 = document.getElementById('button6');
+let colorButtons = [document.getElementById('button1'), document.getElementById('button2'), document.getElementById('button3'), document.getElementById('button4'), document.getElementById('button5'), document.getElementById('button6')];
 let reset = document.getElementById('reset');
 let question = document.getElementById('question');
 let par = document.createElement('p');
@@ -13,27 +8,22 @@ question.appendChild(par);
 
 document.getElementById('status').appendChild(status)
 
-
 reset.addEventListener('click', generate);
-button1.addEventListener('click', button1Check);
-button2.addEventListener('click', button2Check);
-button3.addEventListener('click', button3Check);
-button4.addEventListener('click', button4Check);
-button5.addEventListener('click', button5Check);
-button6.addEventListener('click', button6Check);
-
+colorButtons[0].addEventListener('click', button1Check);
+colorButtons[1].addEventListener('click', button2Check);
+colorButtons[2].addEventListener('click', button3Check);
+colorButtons[3].addEventListener('click', button4Check);
+colorButtons[4].addEventListener('click', button5Check);
+colorButtons[5].addEventListener('click', button6Check);
 
 function randomRgb() {
     return Math.round(Math.random() * (255 - 1) + 1);
 }
 
-
-
 function dynamicColor() {
-    for (let i = 1; i <= 6; i++) {
-        document.getElementById(`button${i}`).style.background = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`
-    }
+    colorButtons.map(item => item.style.background = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`);
 }
+
 dynamicColor()
 
 function chooseWinner() {
@@ -46,100 +36,62 @@ function generate() {
     dynamicColor();
     par.textContent = `Can you find ${chooseWinner()} ?`;
     status.textContent = ``;
-    document.getElementById('button1').removeAttribute('disabled');
-    document.getElementById('button2').removeAttribute('disabled');
-    document.getElementById('button3').removeAttribute('disabled');
-    document.getElementById('button4').removeAttribute('disabled');
-    document.getElementById('button5').removeAttribute('disabled');
-    document.getElementById('button6').removeAttribute('disabled');
+    colorButtons.map(item => item.removeAttribute('disabled'))
 }
 
 par.textContent = `Can you find ${chooseWinner()} ?`;
 
-
-
-
 //list of functions for checking winners
 function button1Check() {
-    if (document.getElementById('button1').style.background == winner) {
+    if (colorButtons[0].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
 
 function button2Check() {
-    if (document.getElementById('button2').style.background == winner) {
+    if (colorButtons[1].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
 
 function button3Check() {
-    if (document.getElementById('button3').style.background == winner) {
+    if (colorButtons[2].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
 
 function button4Check() {
-    if (document.getElementById('button4').style.background == winner) {
+    if (colorButtons[3].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
 
 function button5Check() {
-    if (document.getElementById('button5').style.background == winner) {
+    if (colorButtons[4].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
 
 function button6Check() {
-    if (document.getElementById('button6').style.background == winner) {
+    if (colorButtons[5].style.background == winner) {
         status.textContent = `Congratulations!You found correct color!`;
     } else {
         status.textContent = `Wrong! Try again!`;
     }
-    document.getElementById('button1').disabled = 'false';
-    document.getElementById('button2').disabled = 'false';
-    document.getElementById('button3').disabled = 'false';
-    document.getElementById('button4').disabled = 'false';
-    document.getElementById('button5').disabled = 'false';
-    document.getElementById('button6').disabled = 'false';
+    colorButtons.map(item => item.disabled = 'false');
 }
